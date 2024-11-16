c328c841f4d5de316e6b7c78694546de90721bbd
tomastrela <pikson.tom@gmail.com>
2024-11-16 12:03:15 +0100

    fix calcualte function

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from flask import request
 app = Flask(__name__)
 
 @app.route('/calculate')
-def hello_world():  # put application's code here
+def calculate():
+    """This endpoint calculates opertaion on two arguments"""
     op = request.args.get('op', type=str)
     arg1 = request.args.get('arg1', type=int)
     arg2 = request.args.get('arg2', type=int)
-    result = 0
-    result = calculate(arg1, arg2, op, result)
+
+    result = calculate(arg1, arg2, op)
     return f"{arg1} {op} {arg2} = {result}"
 
-def calculate(arg1, arg2, op, result):
+def calculate(arg1, arg2, op):
+    """Perform operation on two arguments"""
+    result = 0
     match op:
         case 'add':
             result = arg1 + arg2
@@ -23,8 +26,6 @@ def calculate(arg1, arg2, op, result):
         case 'divide':
             if arg2 != 0:
                 result = arg1 / arg2
-    if op == 'add':
-        return arg1 + arg2
     return result
 
 if __name__ == '__main__':
